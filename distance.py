f67f63f3c3bfa66d2095eda766ae06baddd1f7af
wind <aa.veter@gmail.com>
2016-03-14 21:11:29 +0300

    +1

--- distance.py
+++ distance.py
@@ -1,20 +1,30 @@
+# coding: utf-8
+
 class Distance:
 
     def __init__(self, meters=0, km=0):
         self.meters = meters + km * 1000
 
     def __add__(self, b):
-        if type(b) == Distance:
-            self.meters += b.meters
+        if type(b) is Distance:
+            sum_meters = self.meters + b.meters
+        elif type(b) is int:
+            sum_meters = self.meters + b
+        elif type(b) is str:
+            sum_meters = self.meters + int(b)
         else:
-            self.meters += b
-        return self
+            raise TypeError(u'Нельзя так...')
+        return Distance(sum_meters)
 
     def km(self):
         return self.meters / 1000
 
 
 d1 = Distance(300)
+d2 = Distance(600)
+d3 = d1 + '700'
 
 print(d1.meters)
+print(d2.meters)
+print(d3.meters)
 
