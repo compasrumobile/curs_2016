269c20ef3b6c6fbb129bba035692b5ef646279b7
wind <aa.veter@gmail.com>
2016-03-25 19:09:14 +0300

    +1

--- lesson_19.py
+++ lesson_19.py
@@ -21,17 +21,44 @@ import os
 
 # ? доступ к переменным окружения
 
-PATH = os.environ['PATH']
-PATH += ":" + os.path.abspath('.')
-os.environ['PATH'] = PATH
-
-print(os.environ['PATH'])
-
-PATH = os.environ['PATH']
-PATH += ":" + os.path.abspath('.')
-os.environ['PATH'] = PATH
-
-print(os.environ['PATH'])
+# PATH = os.environ['PATH']
+# PATH += ":" + os.path.abspath('.')
+# os.environ['PATH'] = PATH
+#
+# print(os.environ['PATH'])
+#
+# PATH = os.environ['PATH']
+# PATH += ":" + os.path.abspath('.')
+# os.environ['PATH'] = PATH
+#
+# print(os.environ['PATH'])
 
 # ------------------------------------------------------
+
+import sys
+
+
+'''
+
+             1           2
+x = [0000 0101] [0000 0001]
+             2           1
+y = [0000 0001] [0000 0101]
+
+x == y = True
+
+'''
+
+
+# Big Endian
+# Little Endian
+
+print(sys.byteorder) # 'big' или 'little'
+
+
+
+
+
+
+
 
